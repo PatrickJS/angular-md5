75bd41ec05f2c8e462d59e129376be97c5b8196c
Benjamin Orozco <benoror@gmail.com>
2015-07-27 11:40:33 -0500

    refactor: move commonjs supp outside anon func

--- angular-md5.js
+++ angular-md5.js
@@ -2,10 +2,12 @@
   angular-md5 - v0.1.8 
   2015-07-26
 */
+/* commonjs package manager support (eg componentjs) */
+if (typeof module !== "undefined" && typeof exports !== "undefined" && module.exports === exports) {
+  module.exports = "angular-md5";
+}
+
 (function(angular) {
-  if (typeof module !== "undefined" && typeof exports !== "undefined" && module.exports === exports) {
-    module.exports = "angular-md5";
-  }
   angular.module("angular-md5", [ "gdi2290.md5" ]);
   angular.module("ngMd5", [ "gdi2290.md5" ]);
   angular.module("gdi2290.md5", [ "gdi2290.gravatar-filter", "gdi2290.md5-service", "gdi2290.md5-filter" ]);
